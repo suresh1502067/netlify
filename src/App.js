e4a1c11d5071598b4e5646b5abba5871a1548fbb
suresh <suresh1502067@gmail.com>
2022-04-17 14:31:15 +0530

    deploy-test-2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Netlify build is production master-test-1</h1>
+      <h1>Netlify build is production master-test-2</h1>
     </div>
   );
 }
